aeab09d1f6009d9f93e175e1f728b1edf735380a
SALLmouhamadou <salllmouha10@gmail.com>
2023-09-24 00:33:41 +0200

    Mise en place page de compte pour connexion

--- src/pages/Compte.js
+++ src/pages/Compte.js
@@ -2,7 +2,23 @@ import React from "react";
 
 function Compte(){
     return(
-        <h2>Je me connecte ou je m'inscrit</h2>
+        <div className="container shadow-lg p-3 mt-5 mb-5 bg-body-tertiary rounded ">
+            <div className="row">
+                <div className="col">
+                    <h3 className="text-center mt-5 shadow-lg p-3 mb-5 bg-body-tertiary rounded">Je m'inscrit</h3>
+                    <form>
+                        <label>Nom</label>
+                        <input type="text" />
+                    </form>
+                </div>
+                <div className="col">
+                    <h3 className="text-center mt-5 shadow-lg p-3 mb-5 bg-body-tertiary rounded">Je me connecte</h3>
+                    <label>Nom</label>
+                        <input type="text" />
+                </div>
+
+            </div>
+        </div>
     )
 }
 export default Compte;
